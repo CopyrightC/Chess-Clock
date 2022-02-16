d6b37bc8b2f765a493b1fddf2af8fac1011c5421
CopyrightC <shouryasinha001@gmail.com>
2022-02-16 17:34:29 +0530

    Working clock

--- components/PlayScreen.js
+++ components/PlayScreen.js
@@ -2,50 +2,15 @@ import React from 'react';
 import { TimerButton } from './button';
 import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
 export const PlayScreen = () => {
-  const [hrs, setHrs] = React.useState(0);
-  const [mins, setMins] = React.useState(10);
-  const [secs, setSecs] = React.useState(10);
-  const decreaseTime = () => {
-    if (hrs == 0 && mins == 0 && secs == 0);
-    else if (mins == 0 && secs == 0) {
-      setHrs(hrs - 1);
-    } else if (secs == 0) {
-      if (parseInt(mins) === 10) {
-        setMins(`09`);
-      } else {
-        setMins(mins - 1);
-      }
-      setSecs(59);
-    } else {
-      if (parseInt(secs) < 10) {
-        if (!secs.toString().startsWith('0') || secs == '0') {
-          setSecs(`0${parseInt(secs)}`);
-        }
-        if (secs.toString().startsWith('0')) {
-          setSecs(`0${parseInt(secs) - 1}`);
-        }
-      } else {
-        if (parseInt(secs) === 10) {
-          setSecs(`09`);
-        } else {
-          setSecs(secs - 1);
-        }
-      }
-    }
-    if (mins < 10) {
-      if (!mins.toString().startsWith('0') || mins == '0') {
-        setMins(`0${mins}`);
-      }
-    }
-  };
+  const [run, setRun] = React.useState(true);
   React.useEffect(() => {
-    const timerId = setInterval(() => decreaseTime(), 1000);
-    return () => clearInterval(timerId);
+    // console.log('h');
+    // console.log(run);
   });
   return (
     <View style={styles.container}>
-      <TimerButton color="black" fontc="white" h={hrs} m={mins} s={secs} />
-      <TimerButton color="white" fontc="black" h={hrs} m={mins} s={secs} />
+      <TimerButton color="black" fontc="white" prun={!run} psetRun={setRun} />
+      <TimerButton color="white" fontc="black" prun={run} psetRun={setRun} />
     </View>
   );
 };
--- components/button.js
+++ components/button.js
@@ -2,13 +2,60 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
 export const TimerButton = (props) => {
   // alert(props.mins);
+  const [hrs, setHrs] = React.useState(0);
+  const [mins, setMins] = React.useState(10);
+  const [secs, setSecs] = React.useState(`01`);
+  const decreaseTime = () => {
+    if (hrs == 0 && mins == 0 && secs == 0);
+    else if (mins == 0 && secs == 0) {
+      setHrs(hrs - 1);
+    } else if (secs == 0) {
+      if (parseInt(mins) === 10) {
+        setMins(`09`);
+      } else {
+        setMins(mins - 1);
+      }
+      setSecs(59);
+    } else {
+      if (parseInt(secs) < 10) {
+        if (!secs.toString().startsWith('0') || secs == '0') {
+          setSecs(`0${parseInt(secs)}`);
+        }
+        if (secs.toString().startsWith('0')) {
+          setSecs(`0${parseInt(secs) - 1}`);
+        }
+      } else {
+        if (parseInt(secs) === 10) {
+          setSecs(`09`);
+        } else {
+          setSecs(secs - 1);
+        }
+      }
+    }
+    if (mins < 10) {
+      if (!mins.toString().startsWith('0') || mins == '0') {
+        setMins(`0${mins}`);
+      }
+    }
+  };
+  const tapped = () => {
+    if (props.color == 'white' && props.prun) {
+      props.psetRun(false);
+    } else {
+      if (props.prun) props.psetRun(true);
+    }
+  };
+  React.useEffect(() => {
+    if (props.prun) {
+      const timerId = setInterval(() => decreaseTime(), 1000);
+      return () => clearInterval(timerId);
+    }
+  });
   return (
     <View style={{ ...styles.timerButton, backgroundColor: `${props.color}` }}>
-      <TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onPress={() => tapped()}>
         <Text style={{ ...styles.timeText, color: `${props.fontc}` }}>
-          {props.h == 0
-            ? `${props.m}:${props.s}`
-            : `${props.h}:${props.m}:${props.s}`}
+          {hrs == 0 ? `${mins}:${secs}` : `${hrs}:${mins}:${secs}`}
         </Text>
       </TouchableWithoutFeedback>
     </View>
